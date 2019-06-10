f74dc59b873782c0db8d045f5d044b65c3cb1e3c
Manju KC <Manju KC>
2019-06-10 15:55:26 -0400

    create users table

--- knexfile.js
+++ knexfile.js
@@ -4,17 +4,23 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/user.db3',
     },
-   
-   
-   
+    pool: {
+      // SQLite will not enforce FK by default
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
-  migrations: {
-    directory: './database/migrations',
-  },
-
   
 
 };
