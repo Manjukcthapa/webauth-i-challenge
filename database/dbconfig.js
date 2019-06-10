3bf8dd2eb65e6bd251f368bae0d272cf08b74c65
Manju KC <Manju KC>
2019-06-10 17:05:02 -0400

    register and login is working

---
+++ database/dbconfig.js
@@ -0,0 +1,9 @@
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+
+
+
+
+
+module.exports = knex(knexConfig.development);
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const express = require('express');
-const server = express();
-
-server.use(express.json());
+const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
